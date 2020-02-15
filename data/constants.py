496ce5210dc190de6285946458a57f3ba6e21888
Rohan <rohankanchana@github.com>
2020-02-14 19:08:46 -0500

    constants commit bug fix

--- data/constants.py
+++ data/constants.py
@@ -1,5 +1,4 @@
 from processing import sequence_to_one_hots, sequence_to_ordinals
-<<<<<<< HEAD
 from typing import List, Callable, Any, NamedTuple, Dict, Optional
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression, BayesianRidge
@@ -7,11 +6,9 @@ from sklearn.kernel_approximation import Nystroem
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.multioutput import MultiOutputRegressor
-=======
 from typing import List, Callable, Any, NamedTuple, Dict
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression, BayesianRidge
->>>>>>> 62e20d9f88eb1bcd497e60ecef9c8c0beb73207e
 from sklearn import svm
 
 
@@ -29,7 +26,6 @@ RANDOM_STATE: int = 42
 
 
 # Model with method 'fit(X, y)'
-<<<<<<< HEAD
 Model = NamedTuple('Model', [('name', str), ('model', Any), ('shorthand', str), ('preprocess_X', Optional[Callable])])
 
 mlp = MLPRegressor(hidden_layer_sizes=(100, 100, 100, 100, 100, 100), solver='lbfgs', max_iter=50, verbose=True)
@@ -37,16 +33,17 @@ mlp.n_layers_ = 8
 
 
 # feature_map_nystroem = Nystroem(gamma=.2, random_state=1, n_components=10000)
-=======
 Model = NamedTuple('Model', [('name', str), ('model', Any), ('shorthand', str)])
->>>>>>> 62e20d9f88eb1bcd497e60ecef9c8c0beb73207e
+
+asdf: Dict[str, Model] = {
+
+}
 
 
 MODELS: Dict[str, Model] = {
     'RandomForest': Model(
         name='Random Forest',
         model=RandomForestRegressor(n_jobs=-1, bootstrap=True, random_state=RANDOM_STATE, verbose=1),
-<<<<<<< HEAD
         shorthand='rf',
         preprocess_X=None
     ),
@@ -54,20 +51,17 @@ MODELS: Dict[str, Model] = {
         name='Linear Regression',
         model=MultiOutputRegressor(LinearRegression(), n_jobs=-1),
         shorthand='lr',
-        preprocess_X=None
-=======
-        shorthand='rf'
+        preprocess_X=None,
     ),
     'LinearRegression': Model(
         name='Linear Regression',
         model=LinearRegression(n_jobs=-1),
-        shorthand='lr'
->>>>>>> 62e20d9f88eb1bcd497e60ecef9c8c0beb73207e
+        shorthand='lr',
+        preprocess_X=None
     ),
     'BayesianRidge': Model(
         name='Bayesian Ridge',
         model=BayesianRidge(),
-<<<<<<< HEAD
         shorthand='br',
         preprocess_X=None
     ),
@@ -93,15 +87,13 @@ MODELS: Dict[str, Model] = {
         name='Gradient Boosting Regressor',
         model=GradientBoostingRegressor(),
         shorthand='gbr',
-        preprocess_X=None
-=======
+        preprocess_X=None,
         shorthand='br'
     ),
     'SupportVectorMachine': Model(
         name='Support Vector Machine',
         model=svm.SVR(),
         shorthand='svm'
->>>>>>> 62e20d9f88eb1bcd497e60ecef9c8c0beb73207e
     )
 }
 
